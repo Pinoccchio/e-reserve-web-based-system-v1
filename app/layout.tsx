32280b701c4c757e273119bfe6c5af5b18d29e70
pinocchio <jenjiliv@gmail.com>
2025-04-27 22:15:07 +0800

    web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import { RootLayoutClient } from "./components/root-layout-client"
 import { metadata } from "./metadata"
 import type React from "react"
+import { Analytics } from "@vercel/analytics/react" // Import Analytics
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={`${inter.className} flex flex-col min-h-screen bg-gray-50`}>
         <RootLayoutClient>{children}</RootLayoutClient>
+        <Analytics /> {/* Add the Analytics component */}
         <div id="toast-root" />
         <script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
@@ -27,4 +29,3 @@ export default function RootLayout({
     </html>
   )
 }
-
